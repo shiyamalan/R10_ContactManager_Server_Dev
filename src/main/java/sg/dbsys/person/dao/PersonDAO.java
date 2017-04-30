49a0a18350174ee0e338c54a5e9ecfdae18a2b63
Shiyamalan <rsrishiya@gmail.com>
2017-04-30 09:36:58 +0800

    DAO Implementation for accessing the person details

---
+++ src/main/java/sg/dbsys/person/dao/PersonDAO.java
@@ -0,0 +1,14 @@
+package sg.dbsys.person.dao;
+
+import java.util.List;
+
+import sg.dbsys.dao.model.Person;
+
+public interface PersonDAO {
+
+	public void addPerson(Person p);
+	public void updatePerson(Person p);
+	public List<Person> listPersons();
+	public Person getPersonById(int id);
+	public void removePerson(int id);
+}
